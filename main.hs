23db8bd68db35dc40da52d5b299e0dd7796e5898
Jose Alfredo Sitanggang <josealfredositanggang@gmail.com>
2023-12-19 19:07:34 +0700

    sec 3.1

--- main.hs
+++ main.hs
@@ -1,7 +1,23 @@
 module Main where
 
-content :: String
-content = "Hello, World"
+html_ :: String -> String
+html_ child = "<html>" <> child <> "</html>"
+
+head_ :: String -> String
+head_ child = "<head>" <> child <> "</head>"
+
+body_ :: String -> String
+body_ child = "<body>" <> child <> "</body>"
+
+title_ :: String -> String
+title_ child = "<title>" <> child <> "</title>"
+
+makeHtml :: String -> String -> String
+makeHtml t c =
+  html_ $
+    (head_ . title_) t <> body_ c
 
 main :: IO ()
-main = putStrLn content
+main =
+  putStrLn $
+    makeHtml "The Title" "The Body"
